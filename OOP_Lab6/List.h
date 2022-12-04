874b3846c36f1c7f65c4996c47de784bd6e2aa02
rubyRain0 <den015326@gmail.com>
2022-12-04 22:41:45 +0300

    added mergeLists mergeSort implementations.

--- OOP_Lab6/List.h
+++ OOP_Lab6/List.h
@@ -129,6 +129,7 @@ public:
 		head = new NODE<TInfo>(0, nullptr, nullptr);
 		tail = head;
 	}
+	DLIST(const DLIST<TInfo>& other);
 	~DLIST();
 
 	Iterator begin()
@@ -145,6 +146,10 @@ public:
 	{
 		return head;
 	}
+	ptrNODE<ValueType> get_tail()
+	{
+		return tail;
+	}
 	ValueType get_first()
 	{
 		return head->next->info;
@@ -154,15 +159,40 @@ public:
 		return size;
 	}
 
+	#pragma warning (4:4596) 
+	DLIST<TInfo>& DLIST<TInfo>::mergeLists(DLIST<TInfo>::Iterator it1Begin, DLIST<TInfo>::Iterator it1End,
+											DLIST<TInfo>::Iterator it2Begin, DLIST<TInfo>::Iterator it2End);
 
-	void first_node(TInfo data);
+
+
 	bool empty();
 	void add_before(ptrNODE<TInfo> ptr, TInfo data);
 	void add_after(ptrNODE<TInfo> ptr, TInfo data);
 	void print();
 	TInfo& Delete(ptrNODE<TInfo>& ptr);
+
+	void split(ptrNODE<TInfo> head, ptrNODE<TInfo>* a, ptrNODE<TInfo>* b);
+	ptrNODE<TInfo> merge(ptrNODE<TInfo> a, ptrNODE<TInfo> b);
+	void mergeSort(ptrNODE<TInfo> head);
 };
 
+
+template<typename TInfo>
+DLIST<TInfo>& DLIST<TInfo>::mergeLists(DLIST<TInfo>::Iterator it1Begin, DLIST<TInfo>::Iterator it1End,
+	DLIST<TInfo>::Iterator it2Begin, DLIST<TInfo>::Iterator it2End)
+{
+	while (it1Begin != it1End)
+	{
+		(*this).add_after((*this).get_tail(), *it1Begin);
+		it1Begin++;
+	}
+	while (it2Begin != it2End)
+	{
+		(*this).add_after((*this).get_tail(), *it2Begin);
+		it2Begin++;
+	}
+	return *this;
+}
 
 template<typename TInfo>
 bool DLIST<TInfo>::empty()
@@ -232,6 +262,83 @@ TInfo& DLIST<TInfo>::Delete(ptrNODE<TInfo>& ptr)
 	data = p->info;
 	delete p;
 	return data;
+}
+
+template<typename TInfo>
+void DLIST<TInfo>::split(ptrNODE<TInfo> head, ptrNODE<TInfo>* a, ptrNODE<TInfo>* b)
+{
+	ptrNODE<TInfo> slow = head;
+	ptrNODE<TInfo> fast = head->next;
+
+	// advance `fast` by two nodes, and advance `slow` by a single node
+	while (fast != NULL)
+	{
+		fast = fast->next;
+		if (fast != NULL)
+		{
+			slow = slow->next;
+			fast = fast->next;
+		}
+	}
+
+	*b = slow->next;
+	slow->next = NULL;
+}
+
+template<typename TInfo>
+ptrNODE<TInfo> DLIST<TInfo>::merge(ptrNODE<TInfo> a, ptrNODE<TInfo> b)
+{
+	// base cases
+	if (a == NULL) {
+		return b;
+	}
+
+	if (b == NULL) {
+		return a;
+	}
+
+	// pick either `a` or `b`, and recur
+	if (a->info <= b->info)
+	{
+		a->next = merge(a->next, b);
+		a->next->prev = a;
+		a->prev = NULL;
+		return a;
+	}
+	else {
+		b->next = merge(a, b->next);
+		b->next->prev = b;
+		b->prev = NULL;
+		return b;
+	}
+}
+
+template<typename TInfo>
+void DLIST<TInfo>::mergeSort(ptrNODE<TInfo> head)
+{
+	// base case: 0 or 1 node
+	if (head == NULL || head->next == NULL) {
+		return;
+	}
+
+	// split head into `a` and `b` sublists
+	ptrNODE<TInfo> a = head, b = NULL;
+	split(head, &a, &b);
+
+	// recursively sort the sublists
+	mergeSort(a);
+	mergeSort(b);
+
+	// merge the two sorted lists
+	head = merge(a, b);
+}
+
+template<typename TInfo>
+DLIST<TInfo>::DLIST(const DLIST<TInfo>& other)
+{
+	head = new NODE<TInfo>(0, nullptr, nullptr);
+	head = other.head;
+	tail = other.tail;
 }
 
 template<typename TInfo>
--- OOP_Lab6/Source.cpp
+++ OOP_Lab6/Source.cpp
@@ -1,6 +1,4 @@
 #include "List.h"
-#include <typeinfo>
-#include <list>
 int main()
 {
 	DLIST<int> list1;
@@ -8,15 +6,15 @@ int main()
 	list1.add_after(p1, 1);
 	list1.add_after(p1, 2);
 	list1.add_after(p1, 3);
+	list1.add_after(p1, 7);
 
 	DLIST<int> list2;
 	ptrNODE<int> p2 = list2.get_head();
-	list2.add_after(p2, 4);
-	list2.add_after(p2, 5);
+	list2.add_after(p2, 53);
 	list2.add_after(p2, 6);
 
 	DLIST<int>::Iterator it;
-	std::cout << "List1:";
+	std::cout << "List1: ";
 	for (it = list1.begin(); it != list1.end(); it++)
 	{
 		std::cout << *it << " ";
@@ -49,29 +47,30 @@ int main()
 		}
 	}
 
-	std::cout << "\nList1:";
+	std::cout << "\nList1: ";
 	for (it = list1.begin(); it != list1.end(); it++)
 	{
 		std::cout << *it << " ";
 	}
 	
-	std::cout << "\nList2:";
+	std::cout << "\nList2: ";
 	for (it = list2.begin(); it != list2.end(); it++)
 	{
 		std::cout << *it << " ";
 	}
 
 	//Merge List1 & List2 into List3
-	DLIST<int> list3;
-	DLIST<int>::Iterator it1;
-	DLIST<int>::Iterator it2;
-	for (it1 = list1.begin(); it1 != list1.end(); it1++)
+	DLIST<int> listResult;
+	listResult.mergeLists(list1.begin(), list1.end(), list2.begin(), list2.end());
+	std::cout << "\nList3 (Merged): ";
+	for (it = listResult.begin(); it != listResult.end(); it++)
 	{
-		list3.add_after(it1->next, *it1);
+		std::cout << *it << " ";
 	}
 
-	std::cout << "\nList3:";
-	for (it = list3.begin(); it != list3.end(); it++)
+	std::cout << "\nMerge Sort List3: ";
+	listResult.mergeSort(listResult.get_head());
+	for (it = listResult.begin(); it != listResult.end(); it++)
 	{
 		std::cout << *it << " ";
 	}
